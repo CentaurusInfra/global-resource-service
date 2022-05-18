78d6a156dcbec5337d0bf4efe1983a8f7f8fc901
Carl Xie <q131172019@gmail.com>
2022-05-18 13:51:20 -0700

    Codes Improvment based on code reviews after initial PR

--- resource-management/pkg/aggregrator/aggregator.go
+++ resource-management/pkg/aggregrator/aggregator.go
@@ -3,20 +3,19 @@ package aggregrator
 import (
 	"encoding/json"
 	"fmt"
-	"global-resource-service/resource-management/pkg/common-lib/interfaces"
+	"io/ioutil"
+	"net/http"
+	"strings"
+	"time"
+
+	interfaces "global-resource-service/resource-management/pkg/common-lib/interfaces/distributor"
 	"global-resource-service/resource-management/pkg/common-lib/types"
 	"global-resource-service/resource-management/pkg/common-lib/types/event"
-	"io/ioutil"
-	"strings"
-
-	"net/http"
-	"sync"
-	"time"
 )
 
 type Aggregator struct {
-	urls          []string
-	ProcessEvents interfaces.Interface
+	urls           []string
+	EventProcessor interfaces.Interface
 }
 
 type Client struct {
@@ -30,7 +29,8 @@ type Response struct {
 	Length              int
 }
 
-type PullWatchData struct {
+// RRM: Resource Region Manager
+type PullDataFromRRM struct {
 	BatchLength int
 	CRV         types.ResourceVersionMap
 }
@@ -40,50 +40,62 @@ const (
 )
 
 // Initialize aggregator
-func NewAggregator(urls []string, ProcessEvents interfaces.Interface) *Aggregator {
+func NewAggregator(urls []string, EventProcessor interfaces.Interface) *Aggregator {
 	return &Aggregator{
-		urls:          urls,
-		ProcessEvents: ProcessEvents,
+		urls:           urls,
+		EventProcessor: EventProcessor,
 	}
 }
 
 // Main loop to get resources from resource region managers and send to distributor
+// This is only initial code structure for aggregator method
+// TODO:
+//    Based on the speed of process events from resource distributor, dynamic descision of batch length
+//    will be made by aggregator and this batch length will be used to pull resources from resource region manager
 func (a *Aggregator) Run() {
 	//
 	numberOfURLs := len(a.urls)
 
-	var wg sync.WaitGroup
-	wg.Add(numberOfURLs)
+	//var wg sync.WaitGroup
+	//wg.Add(numberOfURLs)
 
 	fmt.Println("Running for loop to connect to to resource region manager...")
 
 	for i := 0; i < numberOfURLs; i++ {
 		go func(i int) {
-			defer wg.Done()
+			//defer wg.Done()
 			var crv types.ResourceVersionMap
 			var minRecordNodeEvents []*event.NodeEvent
 
+			// Connect to resource region manager
+			c := a.createClient(a.urls[i])
+
 			for {
+				time.Sleep(100 * time.Millisecond)
+
 				// Connect to resource region manager
-				c := a.createClient(a.urls[i])
+				//c := a.createClient(a.urls[i])
 
 				// Call the PULL methods, composite RV is nil, the first PULL List. otherwise call the PULL Watch
-				if crv == nil {
-					minRecordNodeEvents, _, _ = a.pullList(c)
-				} else {
-					minRecordNodeEvents, _ = a.pullWatch(c, DefaultBatchLength, crv)
+				minRecordNodeEvents, _ = a.initPullAndSubsequentPull(c, DefaultBatchLength, crv)
+
+				if minRecordNodeEvents != nil {
+					// Call ProcessEvents() and get the CRV from distributor as default success
+					// TODO:
+					//    1. wrap up the logical Node record per Distributor needs ( the interfaces need to be updated as well )
+					//    2. call the ProcessEvents Per RP to unload some cost from the Distributor
+					eventProcess, crv := a.EventProcessor.ProcessEvents(minRecordNodeEvents)
+
+					// Call resource region manager, POST CRV to release old node events when ProcessEvents is successful
+					if eventProcess {
+						a.postCRV(c, crv)
+					}
 				}
-
-				// Call ProcessEvents() and get the CRV from distributor as default success
-				_, crv = a.ProcessEvents.ProcessEvents(minRecordNodeEvents)
-
-				// Call resource region manager, POST CRV to release old node events
-				a.postCRV(c, crv)
 			}
 		}(i)
 	}
 
-	wg.Wait()
+	//wg.Wait()
 
 	fmt.Println("Finished for loop to connect to to resource region manager...")
 }
@@ -98,44 +110,26 @@ func (a *Aggregator) createClient(url string) *Client {
 	}
 }
 
-// Call resource region manager's PULLLIST method {url}/resources/pulllist
-func (a *Aggregator) pullList(c *Client) ([]*event.NodeEvent, types.ResourceVersionMap, int) {
-	path := "c.baseURL/resources/pullList"
-	req, err := http.NewRequest(http.MethodGet, path, nil)
-	if err != nil {
-		fmt.Print(err.Error())
-	}
-	req.Header.Add("Accept", "application/json")
-	req.Header.Add("Content-Type", "application/json")
-	resp, err := c.HTTPClient.Do(req)
-	if err != nil {
-		fmt.Print(err.Error())
+// Call resource region manager's InitPull method {url}/resources/initpull when crv is nil
+// or
+// Call the resource region manager's SubsequentPull method {url}/resources/subsequentpull when crv is not nil
+func (a *Aggregator) initPullAndSubsequentPull(c *Client, batchLength int, crv types.ResourceVersionMap) ([]*event.NodeEvent, int) {
+	var path string
+
+	if crv == nil {
+		path = "c.baseURL/resources/initPull"
+	} else {
+		path = "c.baseURL/resources/subsequentPull"
 	}
 
-	defer resp.Body.Close()
-	bodyBytes, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		fmt.Print(err.Error())
-	}
-
-	var ResponseObject Response
-	json.Unmarshal(bodyBytes, &ResponseObject)
-
-	return ResponseObject.MinRecordNodeEvents, ResponseObject.RvMap, ResponseObject.Length
-}
-
-// Call the resource region manager's PULLWATCH method {url}/resources/pullwatch
-func (a *Aggregator) pullWatch(c *Client, batchLength int, crv types.ResourceVersionMap) ([]*event.NodeEvent, int) {
-	path := "c.baseURL/resources/pullwatch"
-	bytes, _ := json.Marshal(PullWatchData{BatchLength: batchLength, CRV: crv.Copy()})
+	bytes, _ := json.Marshal(PullDataFromRRM{BatchLength: batchLength, CRV: crv.Copy()})
 	req, err := http.NewRequest(http.MethodGet, path, strings.NewReader((string(bytes))))
-
 	if err != nil {
 		fmt.Print(err.Error())
 	}
-
 	req.Header.Add("Accept", "application/json")
 	req.Header.Add("Content-Type", "application/json")
+
 	resp, err := c.HTTPClient.Do(req)
 	if err != nil {
 		fmt.Print(err.Error())
--- resource-management/pkg/common-lib/interfaces/distributor/interfaces.go
+++ resource-management/pkg/common-lib/interfaces/distributor/interfaces.go
@@ -5,6 +5,9 @@ import (
 	"global-resource-service/resource-management/pkg/common-lib/types/event"
 )
 
+// TODO:
+//     Coordinate with Distributor with the interfaces defined here.
+//     Distributor will need to make a minor code changes to switch to the new place
 type Interface interface {
 	RegisterClient(requestedHostNum int) (string, bool, error)
 	ListNodesForClient(clientId string) ([]*types.Node, types.ResourceVersionMap, error)
