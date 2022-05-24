d78069ea0bf36c533565392b2dd817323263a655
Yunwen Bai <yunwen.bai@futurewei.com>
2022-05-24 09:45:10 -0700

    fix UT failures

--- resource-management/pkg/common-lib/types/compositeresourceversion.go
+++ resource-management/pkg/common-lib/types/compositeresourceversion.go
@@ -3,14 +3,6 @@ package types
 import (
 	"global-resource-service/resource-management/pkg/common-lib/types/location"
 )
-//
-//// in case of UT or test uses the klog along with the t.log
-//func init() {
-//	if !flag.Parsed() {
-//		klog.InitFlags(nil)
-//		flag.Parse()
-//	}
-//}
 
 type CompositeResourceVersion struct {
 	RegionId            string
