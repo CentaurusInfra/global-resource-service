31d27642c798cb50adc1a32722d12ad54daaba5e
Yunwen Bai <yunwen.bai@futurewei.com>
2022-05-29 08:28:41 -0700

    add todo on registrerClient interface

--- resource-management/pkg/common-lib/interfaces/distributor/interfaces.go
+++ resource-management/pkg/common-lib/interfaces/distributor/interfaces.go
@@ -6,7 +6,9 @@ import (
 )
 
 type Interface interface {
+	//TODO: revisit to use type Client as parameter
 	RegisterClient(requestedHostNum int, quota types.ResourceQuota, clientInfo types.ClientInfoType) (string, bool, error)
+	
 	ListNodesForClient(clientId string) ([]*types.LogicalNode, types.ResourceVersionMap, error)
 	Watch(clientId string, rvs types.ResourceVersionMap, watchChan chan *event.NodeEvent, stopCh chan struct{}) error
 	ProcessEvents(events []*event.NodeEvent) (bool, types.ResourceVersionMap)
