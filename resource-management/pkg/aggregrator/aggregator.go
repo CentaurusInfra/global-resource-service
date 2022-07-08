9540f85ba36f754737114461d8d06bd754ae5587
Ying Huang <sindica2000@yahoo.com>
2022-07-08 17:53:36 +0000

    Added gate for aggregator checkpoint

--- resource-management/pkg/aggregrator/aggregator.go
+++ resource-management/pkg/aggregrator/aggregator.go
@@ -178,10 +178,12 @@ func (a *Aggregator) initPullOrSubsequentPull(c *ClientOfRRM, batchLength uint64
 	if err != nil {
 		klog.Errorf("Error from JSON Unmarshal:", err)
 	}
-	for i := 0; i < len(ResponseObject.RegionNodeEvents); i++ {
-		for j := 0; j < len(ResponseObject.RegionNodeEvents[i]); j++ {
-			if ResponseObject.RegionNodeEvents[i][j] != nil {
-				ResponseObject.RegionNodeEvents[i][j].SetCheckpoint(metrics.Aggregator_Received)
+	if metrics.ResourceManagementMeasurement_Enabled {
+		for i := 0; i < len(ResponseObject.RegionNodeEvents); i++ {
+			for j := 0; j < len(ResponseObject.RegionNodeEvents[i]); j++ {
+				if ResponseObject.RegionNodeEvents[i][j] != nil {
+					ResponseObject.RegionNodeEvents[i][j].SetCheckpoint(metrics.Aggregator_Received)
+				}
 			}
 		}
 	}
